38d3cf51b80d2e6b2f68da75df4db235e41b6765
Vadim Vaskevich <vadskev@ya.ru>
2024-09-12 21:36:41 +0300

    feat: implement send message to kafka

--- internal/api/rotation/hit.go
+++ internal/api/rotation/hit.go
@@ -2,13 +2,24 @@ package rotation
 
 import (
 	"context"
+	"time"
 
+	"github.com/vadskev/banners-rotation/internal/models"
 	desc "github.com/vadskev/banners-rotation/pkg/rotation_v1"
 )
 
 func (i Implementation) HitBanner(ctx context.Context, request *desc.HitBannerRequest) (*desc.HitBannerResponse, error) {
-	//TODO implement me
-	panic("implement me")
+
+	i.kafkaProducer.SendMessage(
+		models.Message{
+			Type:          "show",
+			SlotID:        12,
+			BannerID:      13,
+			SocialGroupID: 14,
+			CreatedAt:     time.Now().Unix(),
+		})
+
+	return &desc.HitBannerResponse{}, nil
 }
 
 func (i Implementation) SelectBanner(ctx context.Context, request *desc.SelectBannerRequest) (*desc.SelectBannerResponse, error) {
