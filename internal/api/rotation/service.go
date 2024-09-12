9c6a300d5883ece9f4a2d84cb4260a3cf429dc5f
Vadim Vaskevich <vadskev@ya.ru>
2024-09-12 21:37:30 +0300

    feat: implement kafkaProducer to Implementation Server

--- internal/api/rotation/service.go
+++ internal/api/rotation/service.go
@@ -1,6 +1,7 @@
 package rotation
 
 import (
+	"github.com/vadskev/banners-rotation/internal/queue"
 	"github.com/vadskev/banners-rotation/internal/storage"
 	desc "github.com/vadskev/banners-rotation/pkg/rotation_v1"
 )
@@ -8,10 +9,12 @@ import (
 type Implementation struct {
 	desc.UnimplementedRotationServer
 	storageService storage.Storage
+	kafkaProducer  queue.Queue
 }
 
-func NewImplementation(storageService storage.Storage) *Implementation {
+func NewImplementation(storageService storage.Storage, kafkaProducer queue.Queue) *Implementation {
 	return &Implementation{
 		storageService: storageService,
+		kafkaProducer:  kafkaProducer,
 	}
 }
