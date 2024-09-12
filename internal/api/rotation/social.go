405594ea5913f765cfa23985d6aef8dcea228eae
Vadim Vaskevich <vadskev@ya.ru>
2024-09-12 21:40:10 +0300

    feat: implement Social Group Add and Delete / Service

--- internal/api/rotation/social.go
+++ internal/api/rotation/social.go
@@ -2,16 +2,46 @@ package rotation
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/vadskev/banners-rotation/internal/converter"
+	"github.com/vadskev/banners-rotation/internal/logger"
 	desc "github.com/vadskev/banners-rotation/pkg/rotation_v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (i Implementation) AddSocialGroup(ctx context.Context, request *desc.AddSocialGroupRequest) (*desc.AddSocialGroupResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	if len(request.Description) == 0 {
+		return nil, status.Error(codes.Aborted, "description is required")
+	}
+
+	socialGroup, err := i.storageService.AddSocialGroup(ctx, converter.ToSocialGroupFromAddSocialGroupRequest(request))
+	if err != nil {
+		return nil, status.Error(codes.Canceled, err.Error())
+	}
+
+	logger.Info(fmt.Sprintf("inserted banner: %#v", socialGroup))
+
+	return &desc.AddSocialGroupResponse{
+		Id: socialGroup.ID,
+	}, nil
 }
 
 func (i Implementation) DeleteSocialGroup(ctx context.Context, request *desc.DeleteSocialGroupRequest) (*desc.DeleteSocialGroupResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	if request.Id <= 0 {
+		return nil, status.Error(codes.Aborted, "id is required")
+
+	}
+
+	st, err := i.storageService.DeleteSocialGroup(ctx, converter.ToSocialGroupFromSocialGroupRequest(request))
+	if err != nil {
+		return nil, status.Error(codes.NotFound, "social group not found")
+	}
+
+	logger.Info(fmt.Sprintf("deleted social group: %#v", st))
+
+	return &desc.DeleteSocialGroupResponse{
+		Status: "OK",
+	}, nil
 }
